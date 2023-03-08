03c617377da02aa6fe4146f96272f08ed0eba66b
Thiago Guarino <thiago.guarino@outlook.com>
2023-03-08 16:57:23 -0300

    feat: file_management.py - task 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if (path_file.endswith('.txt')) is not True:
+            raise AssertionError
+        with open(path_file, 'r') as file:
+            text = file.read().split("\n")
+            return text
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+    except AssertionError:
+        print("Formato inválido\n", file=sys.stderr)
