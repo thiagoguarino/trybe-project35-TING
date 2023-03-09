063e25cda48d322de0a7d9dc3f17f6ae91dc90a2
Thiago Guarino <thiago.guarino@outlook.com>
2023-03-08 23:45:36 -0300

    feat: file_process.py - task 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,33 @@
+from ting_file_management.file_management import txt_importer
+
+
+# task 3 - thiago guarino
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for dict_elem in instance.queue:
+        if dict_elem["nome_do_arquivo"] == path_file:
+            return None
+
+    text_list = txt_importer(path_file)
+
+    text_data_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(text_list),
+        "linhas_do_arquivo": text_list
+    }
+
+    instance.enqueue(text_data_dict)
+
+    print(
+        f"'nome_do_arquivo': '{text_data_dict['nome_do_arquivo']}'\n",
+        f"'qtd_linhas': {text_data_dict['qtd_linhas']}\n",
+        f"'linhas_do_arquivo': {text_data_dict['linhas_do_arquivo']}\n")
 
 
+# task 4 - thiago guarino
 def remove(instance):
     """Aqui irá sua implementação"""
 
 
+# task 5 - thiago guarino
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
