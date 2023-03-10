1b49795090673507eba4528748d314690b879558
Thiago Guarino <thiago.guarino@outlook.com>
2023-03-09 23:55:48 -0300

    feat: word_search.py) exists_word function - task 7

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,6 @@
 # from ting_file_management.priority_queue import PriorityQueue
 
 
+# task 6 - thiago guarino
 def test_basic_priority_queueing():
     """Aqui irá sua implementação"""
--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,22 @@
+# task 7 - thiago guarino
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    results_list = list()
+    for index in range(len(instance)):
+        word_count_list = list()
+        text_data_dict = instance.search(index)
+        for line_index, line in enumerate(text_data_dict["linhas_do_arquivo"]):
+            if word.casefold() in line.casefold():
+                word_count_list.append({"linha": line_index + 1})
+        result_data_dict = {
+            "palavra": word,
+            "arquivo": text_data_dict["nome_do_arquivo"],
+            "ocorrencias": word_count_list
+        }
+        if len(result_data_dict["ocorrencias"]) > 0:
+            results_list.append(result_data_dict)
+    return results_list
 
 
+# task 8 - thiago guarino
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
