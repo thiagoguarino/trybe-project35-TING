66b7773f90304f782f793ca40ba682f995acdf4e
Thiago Guarino <thiago.guarino@outlook.com>
2023-03-10 00:22:53 -0300

    feat: word_search.py) search_by_word function - task 8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -19,4 +19,22 @@ def exists_word(word, instance):
 
 # task 8 - thiago guarino
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    results_list = list()
+    for index in range(len(instance)):
+        word_count_list = list()
+        text_data_dict = instance.search(index)
+        for line_index, line in enumerate(text_data_dict["linhas_do_arquivo"]):
+            if word.casefold() in line.casefold():
+                word_count_list.append(
+                    {
+                        "linha": line_index + 1,
+                        "conteudo": line
+                    })
+        result_data_dict = {
+            "palavra": word,
+            "arquivo": text_data_dict["nome_do_arquivo"],
+            "ocorrencias": word_count_list
+        }
+        if len(result_data_dict["ocorrencias"]) > 0:
+            results_list.append(result_data_dict)
+    return results_list
